ff64379da11b5239c8d009d3f4bf2cd0887c17d6
Jafeth1110 <jafethespinoz@gmail.com>
2025-11-07 20:57:24 -0600

    Fix server variables for API routing

--- index.php
+++ index.php
@@ -10,5 +10,10 @@
 // Change current directory to public
 chdir(__DIR__ . '/public');
 
+// Fix server variables for Laravel routing
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
 // Forward to public/index.php
 require __DIR__ . '/public/index.php';
